a1ab22fe4b2ca81b670665d9f6f277d29a9581da
Ivan Shirov <shirov.ivan101@gmail.com>
2022-03-06 22:36:32 +0500

    firstCommit

---
+++ bboard/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bd
+
+
+# Create your views here.
+
+def index(request):
+    bbs = Bd.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
